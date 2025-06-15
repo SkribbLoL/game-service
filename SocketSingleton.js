17f64ff873129de10e99262047e7620341c8769c
Škrbić Veljko <494716@student.fontys.nl>
2025-06-15 22:24:39 +0200

    Changing the URL for ws

--- SocketSingleton.js
+++ SocketSingleton.js
@@ -30,7 +30,7 @@ class SocketSingleton {
         origin: '*', // In production, restrict this to your frontend URL
         methods: ['GET', 'POST'],
       },
-      path: '/game/socket.io/',
+      path: '/socket.io/',
       allowEIO3: true,
     });
 
